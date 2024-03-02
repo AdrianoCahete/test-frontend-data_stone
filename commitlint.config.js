006bcca596b7f469f1d3da7e85cb52ef3de0b460
Adriano Cahete <me@adrianocahete.dev>
2024-03-02 15:29:19 -0300

    test(root): add commit rules + docs

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,2 +1,28 @@
 // https://commitlint.js.org/guides/getting-started
-export default { extends: ['@commitlint/config-conventional'] }
+export default {
+  extends: ['@commitlint/config-conventional'],
+
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'cosmetic',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert'
+      ]
+    ],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'scope-enum': [2, 'always', ['app', 'actions', 'docs', 'root', 'release', 'setup']],
+    'scope-empty': [2, 'never']
+  }
+}
