b75cd15daddf62c7bd2e452489c16e9c1eac3c29
Adriano Cahete <me@adrianocahete.dev>
2024-03-07 00:26:27 -0300

    chore(setup): add basic install files per os + bump version

---
+++ setup/install.sh
@@ -0,0 +1,14 @@
+# Check if node is installed and it's version
+## Install if not
+## Check if nvm is installed. Install if not
+### Ask to install node if not using nvm
+
+# Check if already has a package-lock.json and node_modules folder
+## Install dependencies if not
+npm install
+
+# Check if vscode is installed. Recommend with winget command if not.
+# Ask to open VSCode. Proccess if yes, go to next if not.
+
+# Give recommendation command to run project.
+npm run dev
